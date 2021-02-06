a2c7fdcc67feacd09bc1c6fcbaae41dd5c262627
victormrs <victorsanmorais@gmail.com>
2021-02-05 21:53:17 -0300

    Criados atributo complexo pais

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,24 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=1):
+    def __init__(self, *pais, nome=None, idade=1):
         self.nome = nome
         self.idade = idade
+        self.pais = list(pais)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Victor', 21)
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.nome)
-    print(p.idade)
-    print(p.cumprimentar())
-    print(f'{p.nome}, {p.idade}')
+    aldo = Pessoa(nome='Aldo', idade=48)
+    karla = Pessoa(nome='Karla', idade=45)
+    victor = Pessoa(aldo, karla, nome='Victor', idade=21)
+    print(Pessoa.cumprimentar(victor))
+    print(id(victor))
+    print(victor.nome)
+    print(victor.idade)
+    print(victor.cumprimentar())
+    print(f'{victor.nome}, {victor.idade}')
+    print(victor.pais)
+    for pai in victor.pais:
+        print(pai)
 
