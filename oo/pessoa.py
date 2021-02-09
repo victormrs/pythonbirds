47b480a58f702c7b6cf4fb2e7ec60a1de09a5e62
victormrs <victorsanmorais@gmail.com>
2021-02-09 17:29:19 -0300

    Criado metodo estatico e metodo de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos = 2
+
     def __init__(self, *pais, nome=None, idade=1):
         self.nome = nome
         self.idade = idade
@@ -7,6 +9,13 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 40
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls}, -olhos {cls.olhos}'
 
 if __name__ == '__main__':
     aldo = Pessoa(nome='Aldo', idade=48)
@@ -25,4 +34,9 @@ if __name__ == '__main__':
     del victor.sobrenome
     print(aldo.__dict__)
     print(victor.__dict__)
-
+    print(Pessoa.olhos)
+    print(aldo.olhos)
+    print(victor.olhos)
+    print(id(Pessoa.olhos), id(aldo.olhos), id(victor.olhos))
+    print(Pessoa.metodo_estatico(), victor.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), victor.nome_e_atributos_de_classe())
